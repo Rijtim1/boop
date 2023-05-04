0bd88607099b11b650b63793c1cfcfc30555f960
Rijan Timsina <regiontimsina@gmail.com>
2023-05-04 14:10:13 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,39 +3,15 @@ import logo from "./static-files/logo.png";
 
 export default function Header() {
   return (
-    <header className="bg-white border-b border-gray-200">
+    <header className="bg-gray border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
+            <a href="/boop">
               <span className="sr-only">Workflow</span>
-              <img className="h-8 w-auto sm:h-10" src={logo} alt="Official logo of the company" />
+              <img className="h-8 w-auto sm:h-10" src={logo} alt="Official logo of the company" width={100} height={100}/>
             </a>
           </div>
-          <div className="-mr-2 -my-2 md:hidden">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-              aria-label="Menu"
-            >
-              <svg
-                className="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <span className="sr-only">Open menu</span>
-            </button>
-          </div>
-
-          <nav className="hidden md:flex space-x-10"></nav>
         </div>
       </div>
     </header>
