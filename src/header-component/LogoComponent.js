44ca50956f85bb6a8041073b0f3ac0858ef2b80f
Rijan Timsina <regiontimsina@gmail.com>
2023-05-05 09:03:50 -0400

    Create LogoComponent.js

---
+++ src/header-component/LogoComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from "../static-files/logo.png"
+
+const Logo = () => {
+  return (
+    <img src={logo} alt="logo" className="h-8 w-8" />
+  );
+};
+
+export default Logo;
