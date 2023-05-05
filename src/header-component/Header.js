625f7c683ff0c49936ac237a2de492a95cc198ca
Rijan Timsina <regiontimsina@gmail.com>
2023-05-05 09:08:21 -0400

    Added support for fixing the header, but still working on implementation.

--- src/header-component/Header.js
+++ src/header-component/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from './LogoComponent';
 import Search from './Search';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="bg-gray-900">
+    <header className="bg-gray-900 fixed-header">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <Logo />
