a3a5c4c5045341c7d6a7d817cc99fa3fb202e034
Rijan Timsina <regiontimsina@gmail.com>
2023-05-05 09:03:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Boop from "./boop-component/Boop"
+import Boop from "./boop-component/Boop";
 import CreateBoop from "./CreateBoop";
+import Header from "./header-component/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Boop />
       <CreateBoop />
     </>
